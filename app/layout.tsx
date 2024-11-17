788b4913c9ad451271974b0561a035e20bf2e5ec
Martin Lowe <martinlowe673@gmail.com>
2024-11-17 15:50:04 +0000

    main tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,10 @@ export default function RootLayout({
       <body
         className={inter.className}
       >
+        <main className='mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8'>
+   
         {children}
+        </main>
       </body>
     </html>
   );
