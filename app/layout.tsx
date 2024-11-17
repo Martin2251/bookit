b1aebb4c85424cc32a5cbacbd77bdde63079f404
Martin Lowe <martinlowe673@gmail.com>
2024-11-17 15:48:14 +0000

    inter

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={inter.className}
       >
         {children}
       </body>
