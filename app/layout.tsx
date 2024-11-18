5fae4252a650ff4b69e940df353168fe89ee7886
Martin Lowe <martinlowe673@gmail.com>
2024-11-18 07:41:10 +0000

    header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import {Inter} from "next/font/google";
 import "@/assets/style/globals.css";
+import Header from "@/components/Header";
 
 const inter = Inter();
 
@@ -21,6 +22,7 @@ export default function RootLayout({
       <body
         className={inter.className}
       >
+        <Header  />
         <main className='mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8'>
    
         {children}
